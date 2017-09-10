47366fd980ec0578e17bd61c08d3101a5337829b
sixsixfive <punx665@gmail.com>
2017-09-10 17:00:26 +0200

    Sun Sep 10 17:00:26 CEST 2017

---
+++ @extra/appthemes/qt5ct/0nyX/qss/0nyX_changeprefix.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+basedir="$(dirname "$(readlink -f "${0}")")"
+cd "$basedir"
+_prefix=$(yad --title="Set Prefix" --on-top --center --text="Change Prefix" --image="dialog-question" --entry --entry-label="Prefix:" --entry-text="/usr" --button="gtk-ok:0")
+for f in $basedir/*.qss; do sed -i 's|url("/usr|url("'$(echo $_prefix)'|g' $f;done
+sed -i 's|/usr|'$(echo $_prefix)'|g' 0nyX_changeprefix.sh
