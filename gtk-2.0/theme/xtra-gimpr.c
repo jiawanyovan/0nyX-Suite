9bbdb6972da574c73ea7997eb0c760f7c0e517cf
sixsixfive <punx665@gmail.com>
2017-10-08 02:36:32 +0200

    Sun Oct  8 02:36:32 CEST 2017

--- gtk-2.0/theme/xtra-gimpr.c
+++ gtk-2.0/theme/xtra-gimpr.c
@@ -11,3 +11,10 @@ style"gimp3"{
 xthickness=0
 ythickness=4}
 widget"*GimpStatusbar*GtkFrame" style"gimp3"
+
+style"gimp4"{
+stock["gimp-menu-left"] = {{ "../@extra/icons/0nyX/symbolic/open-menu.svg" }}
+stock["gimp-menu-right"] = {{ "../@extra/icons/0nyX/symbolic/open-menu.svg" }}
+stock["dockwidget-close"] = {{ "../@extra/icons/0nyX/symbolic/dockwidget-close.svg" }}
+stock["gimp-close"] = {{ "../@extra/icons/0nyX/symbolic/dockwidget-close.svg" }}}
+class "*" style "gimp4"
