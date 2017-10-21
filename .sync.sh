f2fe1bb448e362699cb70d8a08019fbd3de2d22f
ssf <ssf@debian>
2017-10-21 09:46:55 +0200

    Sa 21. Okt 09:46:55 CEST 2017

--- .sync.sh
+++ .sync.sh
@@ -113,6 +113,6 @@ MESSAGE="$(date)"
 git commit -m "$MESSAGE"
 #git remote add origin https://github.com/sixsixfive/0nyX.git
 #git push -u --force origin master
-git push origin master
+git push origin master --force
 printf "\n...done\n"
 exit
