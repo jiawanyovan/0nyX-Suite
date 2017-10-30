192ab0e28a99dad1308551eaf231e6e715921923
ssf <ssf@debian>
2017-10-30 14:54:46 +0100

    Mo 30. Okt 14:54:46 CET 2017

--- @extra/scripts/setxfcetheme.sh
+++ @extra/scripts/setxfcetheme.sh
@@ -23,9 +23,10 @@ if type xfconf-query >/dev/null 2>&1; then
 	xfconf-query -c xsettings -p /Gtk/ButtonImages -s false || true
 	xfconf-query -c xsettings -p /Gtk/MenuImages -s true || true
 	xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "0nyX" || true
-	xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "0nyX" || true
 	xfconf-query -c xsettings -p /Gtk/ToolbarIconSize -s "3" || true
 	xfconf-query -c xsettings -p /Gtk/MenuImages -s true || true
+	xfconf-query -c xsettings -p /Gtk/IconSizes -s 'gtk-menu=16,16:gtk-small-toolbar=24,24:gtk-large-toolbar=24,24:gtk-dnd=48,48:gtk-button=24,24:gtk-dialog=48,48:gtk-panel=24,24:panel-applications-menu=24,24:panel-tasklist-menu=32,32:panel-menu=24,24:panel-directory-menu=24,24:panel-launcher-menu=24,24:panel-window-menu=24,24:panel-menu-bar=24,24:ev-icon-size-annot-window=16,16:webkit-media-button-size=24,24'
+	xfconf-query -c xsettings -p /Gtk/ColorPalette -s 'black:#252424:#323131:#F1F2F2:white:#BB2847:#BB2835:#BB286A:#BB28B0:#8428BB:#285CBB:#288ABB:#28B6BB:#28BB70:#5DBB28:#A2BB28:#BABB28:#BB9628:#BB6F28:#BB4E28'
 	xfconf-query -c xfwm4 -p /general/theme -s "0nyX" || true
 	xfconf-query -c xfce4-notifyd -p /theme -s "0nyX" || true
 	xfconf-query -c xfce4-session -p /splash/Engine -s "balou" || true
